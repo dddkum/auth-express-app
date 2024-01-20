4098323d123986553c5e8220134071d01bb17fe8
Dmitry Kumachev <dkumachev18@gmail.com>
2024-01-21 02:05:41 +0400

    fix axios interceptor

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { useAuthStore } from "../store/auth";
 
 export const BASE_URL = 'http://localhost:5000'
 
@@ -8,7 +9,8 @@ const $api = axios.create({
 })
 
 $api.interceptors.request.use((config) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    const token = useAuthStore.getState().token;
+    config.headers.Authorization = `Bearer ${token}`;
     return config;
 })
 
