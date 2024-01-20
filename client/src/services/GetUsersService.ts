36c195edea77f9a37e3bb88bd0a68e02de205adb
Dmitry Kumachev <dkumachev18@gmail.com>
2024-01-21 00:56:53 +0400

    add get users function

---
+++ client/src/services/GetUsersService.ts
@@ -0,0 +1,10 @@
+import $api from "../api/api";
+
+export const getUsers = async () => {
+    try {
+        const response = await $api.get('/api/users');
+        return response.data;
+    } catch (error) {
+        console.error('get users error', error);
+    }
+};
