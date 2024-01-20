27a1092243bae300a7d599a3fac3b04477c8ce5f
Dmitry Kumachev <dkumachev18@gmail.com>
2024-01-20 20:00:12 +0400

    add generate and save tokens methods

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -1,5 +1,27 @@
-class TokenService {
+const jwt = require('jsonwebtoken');
+const tokenModel = require('../models/token-model');
 
+const secretAccessTokenKey = process.env.JWT_ACCESS_SECRET_KEY;
+const secretRefreshTokenKey = process.env.JWT_REFRESH_SECRET_KEY;
+
+class TokenService {
+    generateTokens(payload) {
+        const accessToken = jwt.sign(payload, secretAccessTokenKey, {expiresIn: '1h'})
+        const refreshToken = jwt.sign(payload, secretRefreshTokenKey, {expiresIn: '30d'})
+        return {
+            accessToken,
+            refreshToken
+        }
+    }
+
+    async saveToken(userId, refreshToken) {
+        const tokenData = await tokenModel.findOne({user: userId})
+        if(tokenData) {
+            tokenData.refreshToken = refreshToken
+            return tokenData.save();
+        }
+        await tokenModel.create({user: userId, refreshToken})
+    }
 }
 
 module.exports = new TokenService();
