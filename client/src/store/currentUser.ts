207019a1f474352f03c3b45cf5b6194777af3542
Dmitry Kumachev <dkumachev18@gmail.com>
2024-01-21 02:16:35 +0400

    edit current user store

--- client/src/store/currentUser.ts
+++ client/src/store/currentUser.ts
@@ -3,16 +3,20 @@ import { create } from "zustand";
 type CurrentUserStore = {
     email: string;
     password: string;
+    isAdmin: boolean;
     setEmail: (currentLogin: string) => void;
     setPassword: (currentPassword: string) => void;
+    setIsAdmin: (isAdmin: boolean) => void;
     reset: () => void;
 };
 
 export const useCurrentUserStore = create<CurrentUserStore>((set) => ({
     email: "",
     password: "",
+    isAdmin: false,
     setEmail: (email: string) => set({ email }),
     setPassword: (password: string) => set({ password }),
+    setIsAdmin: (isAdmin: boolean) => set({isAdmin}),
     reset: () =>
         set({
             email: "",
