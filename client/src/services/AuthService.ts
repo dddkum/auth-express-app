ff8afa99fb17b6fadb112bd5444b2a88936f8579
Dmitry Kumachev <dkumachev18@gmail.com>
2024-01-21 01:22:31 +0400

    add authorization methods

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -1,28 +1,36 @@
 import $api from '../api/api';
+import {useAuthStore} from "../store/auth";
 
 export const register = async (email: string, password: string) => {
     try {
         const response = await $api.post('/api/register', { email, password });
-        return response.data;
+        console.log(response.data)
     } catch (error) {
         console.error('register error', error);
     }
 };
 
 export const login = async (email: string, password: string) => {
+    const authStore = useAuthStore.getState()
     try {
         const response = await $api.post('/api/login', { email, password });
-        return response.data;
+        authStore.setToken(response.data.accessToken)
     } catch (error) {
-        console.error('login error', error);
+        console.error('login error', error)
+        authStore.setIsAuthorized(false)
+    } finally {
+        authStore.setIsAuthorized(true)
     }
 };
 
 export const logout = async () => {
+    const authStore = useAuthStore.getState()
     try {
         const response = await $api.post('/api/logout');
-        return response.data;
+        console.log(response.data)
     } catch (error) {
         console.error('logout error', error);
+    } finally {
+        authStore.reset()
     }
 };
