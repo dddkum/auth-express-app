43fdc0288b420a60654871fc7b00641efa7d5545
Dmitry Kumachev <dkumachev18@gmail.com>
2024-01-24 18:15:03 +0400

    edit app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,23 @@
 import "./App.scss";
 import LoginForm from "./components/Login/LoginForm";
+import {useEffect} from "react";
+import {checkAuth, logout} from "./services/AuthService";
+import {useAuthStore} from "./store/auth";
+import {useCurrentUserStore} from "./store/currentUser";
 
 function App() {
+
+    const {isAuthorized} = useAuthStore();
+    const {user} = useCurrentUserStore();
+
+    useEffect(() => {
+        checkAuth()
+    }, [])
+
     return (
         <div>
-            <LoginForm/>
+            <h1>{isAuthorized ? `Пользователь авторизован ${user?.email}` : <LoginForm/>}</h1>
+            <button onClick={logout}>Выход</button>
         </div>
     )
 }
