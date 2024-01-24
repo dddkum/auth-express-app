517d08765337f979e7eaa074904de34c61b8cd47
Dmitry Kumachev <dkumachev18@gmail.com>
2024-01-24 18:14:05 +0400

    add check authorization method

--- client/src/services/AuthService.ts
+++ client/src/services/AuthService.ts
@@ -1,5 +1,8 @@
 import $api, {API_URL} from '../api/api';
 import {useAuthStore} from "../store/auth";
+import axios from "axios";
+import {AuthResponse} from "../models/response/AuthResponse";
+import {useCurrentUserStore} from "../store/currentUser";
 
 export const register = async (email: string, password: string) => {
     const authStore = useAuthStore.getState();
@@ -40,3 +43,15 @@ export const logout = async () => {
         authStore.reset()
     }
 };
+
+export const checkAuth = async () => {
+    const authStore = useAuthStore.getState();
+    try {
+        const response = await axios.get<AuthResponse>(`${API_URL}/refresh`, {withCredentials: true});
+        console.log(response);
+        authStore.setToken(response.data.accessToken)
+        authStore.setIsAuthorized(true);
+    } catch (error) {
+        console.log(error.response?.data?.message)
+    }
+}
