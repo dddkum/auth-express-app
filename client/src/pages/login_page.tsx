cb9a73dcae914a1e83c25dbea18fb81440432b21
Dmitry Kumachev <dkumachev18@gmail.com>
2024-01-25 17:33:22 +0400

    add login and main page

---
+++ client/src/pages/login_page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "../components/Login/auth-form";
+
+function LoginPage() {
+    return (
+        <LoginForm/>
+    )
+}
+
+export default LoginPage;
---
+++ client/src/pages/main_page.tsx
@@ -0,0 +1,6 @@
+function MainPage() {
+    return (
+        <div></div>
+    )
+}
+export default MainPage;
