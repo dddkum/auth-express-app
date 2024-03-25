b52d114e066f55634a8cbb037aa5fcb801cf3645
Dmitry Kumachev <dkumachev18@gmail.com>
2024-03-26 01:06:03 +0400

    feat: using react-hook-form to register auth form

--- client/src/components/Login/auth-form.tsx
+++ client/src/components/Login/auth-form.tsx
@@ -1,52 +1,75 @@
-import { useState } from 'react'
-import { submitLogin, submitRegister } from './helpers'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
+interface IFormInput {
+    email: string
+    password: string
+}
 function LoginForm() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IFormInput>()
+    const handleSubmitForm: SubmitHandler<IFormInput> = data =>
+        console.log(data)
 
     return (
-        <div className="card border-0 rounded-lg bg-dark-secondary m-auto w-50">
-            <div className="card-body">
-                <form>
-                    <div className="form-floating mb-3">
-                        <input
-                            className="form-control"
-                            id="inputEmail"
-                            type="email"
-                            placeholder="email@example.com"
-                            onChange={e => setEmail(e.target.value)}
-                            value={email}
-                        />
-                        <label htmlFor="inputEmail">Email</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input
-                            className="form-control"
-                            id="inputPassword"
-                            type="password"
-                            placeholder="Password"
-                            onChange={e => setPassword(e.target.value)}
-                            value={password}
-                        />
-                        <label htmlFor="inputPassword">Пароль</label>
-                    </div>
-                    <div className="d-flex align-items-center justify-content-between mt-4 mb-0">
-                        <button
-                            className="btn btn-primary"
-                            onClick={e => submitLogin(e, email, password)}
-                        >
-                            Вход
-                        </button>
-                        <button
-                            className="btn btn-dark-primary ms-2"
-                            onClick={e => submitRegister(e, email, password)}
-                        >
-                            Регистрация
-                        </button>
-                    </div>
-                </form>
-            </div>
+        <div className="d-flex align-items-center justify-content-center vh-100">
+            <form
+                onSubmit={handleSubmit(handleSubmitForm)}
+                className="d-flex flex-column w-50"
+            >
+                <label htmlFor="email">Email</label>
+                <input
+                    {...register('email', {
+                        required: {
+                            value: true,
+                            message: 'Email is required',
+                        },
+                        pattern: {
+                            value: /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}$/,
+                            message: 'Invalid email address',
+                        },
+                    })}
+                />
+                <span className="text-danger">{errors.email?.message}</span>
+
+                <label htmlFor="password" className="mt-3">
+                    Пароль
+                </label>
+                <input
+                    {...register('password', {
+                        required: {
+                            value: true,
+                            message: 'Password is required',
+                        },
+                        minLength: {
+                            value: 8,
+                            message:
+                                'Password must be at least 8 characters long',
+                        },
+                        pattern: {
+                            value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                            message:
+                                'Password must have at least one uppercase letter, one lowercase letter, one digit, and one special character',
+                        },
+                    })}
+                />
+                <span className="text-danger">{errors.password?.message}</span>
+
+                <button type="submit" className="btn btn-outline-warning mt-5">
+                    Вход
+                </button>
+                <button
+                    type="button"
+                    className="btn btn-warning mt-2"
+                    onClick={() =>
+                        console.log('register button need to configure')
+                    }
+                >
+                    Регистрация
+                </button>
+            </form>
         </div>
     )
 }
