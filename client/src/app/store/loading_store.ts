47faef939978ed605da27974c91dbb324ad35d85
Dmitry Kumachev <dkumachev18@gmail.com>
2024-03-30 02:59:29 +0400

    clean: delete unused/unnecessary code

--- client/src/app/store/loading_store.ts
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand'
-import { LoadingStore } from '../types/ILoading.ts'
-
-export const useLoadingStore = create<LoadingStore>(set => ({
-    isLoading: false,
-    setIsLoading: isLoading => set({ isLoading }),
-}))
