c21739981ef315596ee71563266012d4b238c3c1
Dmitry Kumachev <dkumachev18@gmail.com>
2024-03-30 00:43:32 +0400

    ci: edit axios config

--- client/src/app/api/api.ts
+++ client/src/app/api/api.ts
@@ -8,10 +8,43 @@ const $api = axios.create({
     baseURL: API_URL,
 })
 
-$api.interceptors.request.use(config => {
-    const token = useAuthStore.getState().token
-    config.headers.Authorization = `Bearer ${token}`
-    return config
-})
+$api.interceptors.request.use(
+    config => {
+        const token = useAuthStore.getState().token
+        config.headers.Authorization = `Bearer ${token}`
+        return config
+    },
+    error => Promise.reject(error)
+)
+
+$api.interceptors.response.use(
+    response => {
+        if (response.headers && response.headers['authorization']) {
+            const token = response.headers['authorization'].split('Bearer ')[1]
+            useAuthStore.getState().setToken(token)
+        }
+
+        return response
+    },
+    error => {
+        if (!error) {
+            return Promise.reject(error)
+        }
+
+        if (error.response.headers && error.response.headers['authorization']) {
+            const token =
+                error.response.headers['authorization'].split('Bearer ')[1]
+            useAuthStore.getState().setToken(token)
+        }
+
+        switch (error.response.status) {
+            case 401:
+                useAuthStore.getState().reset()
+                break
+        }
+
+        return Promise.reject(error)
+    }
+)
 
 export default $api
