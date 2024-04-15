1cb5c19ff1166b06ba8c6476636365cea854153f
Dmitry Kumachev <dkumachev18@gmail.com>
2024-04-16 00:55:59 +0400

    feat: add improved todos-model, rewriting and sorting todos in the database by ID number

--- server/models/todos-model.js
+++ server/models/todos-model.js
@@ -1,9 +1,15 @@
 const { Schema, model } = require("mongoose");
 
-const TodosSchema = new Schema({
-  todo: { type: String },
-  noteDate: { type: Date },
-  deadLineDate: { type: Date },
-});
+const TodosSchema = new Schema(
+    {
+      id: { type: Number, unique: true, required: true },
+      todo: { type: String, required: true },
+      noteDate: { type: Date, required: true },
+      deadLineDate: { type: Date, required: true },
+    },
+    {
+      versionKey: false,
+    }
+);
 
 module.exports = model("Todos", TodosSchema);
--- server/service/todos-service.js
+++ server/service/todos-service.js
@@ -2,14 +2,49 @@ const Todos = require("../models/todos-model");
 
 class TodosService {
   async getTodos() {
-    const todos = await Todos.find({});
-    return todos;
+    const todos = await Todos.find({}).select('todo noteDate deadLineDate id').exec();
+    return todos.map((todo) => ({
+      todo: todo.todo,
+      noteDate: todo.noteDate,
+      deadLineDate: todo.deadLineDate,
+      id: todo.id,
+    }));
   }
 
-  async postTodo(todoData){
+  async reorderTodos() {
+    const todos = await Todos.find().sort({ id: 1 }).exec();
+
+    for (let i = 0; i < todos.length; i++) {
+      const todo = todos[i];
+
+      if (todo.id !== i + 1) {
+        // если есть пропуски в числовом порядке id, необходимо переупорядочить и перезаписать документы
+        for (let j = 0; j < todos.length; j++) {
+          const todoToUpdate = todos[j];
+          todoToUpdate.id = j + 1;
+          await todoToUpdate.save();
+        }
+        break;
+      }
+    }
+  }
+
+  async postTodo(todoData) {
+    const todos = await Todos.find().exec();
+    const lastTodo = todos[todos.length - 1];
+
+    if (lastTodo) {
+      todoData.id = lastTodo.id + 1;
+    } else {
+      todoData.id = 1;
+    }
+
     const todo = await Todos.create(todoData);
+    await this.reorderTodos();
+
     return todo;
   }
 }
 
 module.exports = new TodosService();
+
